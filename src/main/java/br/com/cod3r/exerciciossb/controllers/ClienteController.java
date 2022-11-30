6b29f218f8f9f0641e10989caf3eccd7158d44d0
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-30 13:33:52 -0300

    feat: passando parâmetro pela url

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -1,5 +1,24 @@
 package br.com.cod3r.exerciciossb.controllers;
 
-public class ClienteController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cod3r.exerciciossb.models.Cliente;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	@GetMapping(path = "/qualquer")
+	public Cliente obterCliente() {
+		return new Cliente(2, "Cristian", "4923492321");
+	}
+	
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Crirstian", "43242343242");
+	}
 }
--- src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -1,13 +1,15 @@
 package br.com.cod3r.exerciciossb.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrimeiroController {
-	
-	@RequestMapping(method = RequestMethod.GET)
+	/**
+	 *  GetMapping e RequestMapping(method = RequestMethod.GET funcionam da mesma forma;
+	 * @return
+	 */
+	@GetMapping()
 	public String ola() {
 		return "Olá Spring Boot!";
 	}
