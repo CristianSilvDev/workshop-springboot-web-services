71a01ad3092221ad805d34aa52331e73c4345199
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-12-01 11:26:15 -0300

    feat: implementando atributos

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,19 +1,25 @@
 package br.com.cod3r.exerciciossb.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exerciciossb.model.entities.Produto;
+import br.com.cod3r.exerciciossb.model.repositories.ProdutoRepository;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	@PostMapping
-	public Produto novoProduto(@RequestParam String nome) {
-		Produto produto = new Produto(nome);
+	public Produto novoProduto(@RequestParam String nome, @RequestParam double preco, @RequestParam double desconto) {
+		Produto produto = new Produto(nome, preco, desconto);
+		produtoRepository.save(produto);
 		return produto;
 	}
 }
