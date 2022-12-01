63cf70b49b1780ab08f41b5a3c54b3507bb5d838
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-12-01 11:48:10 -0300

    feat: consultando todos os produtos

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,12 +1,14 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exerciciossb.model.entities.Produto;
 import br.com.cod3r.exerciciossb.model.repositories.ProdutoRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -16,8 +18,13 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 
 	@PostMapping
-	public Produto novoProduto(Produto produto) {
+	public Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
+	}
+	
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
 	}
 }
